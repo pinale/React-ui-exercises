b50b6c06fd126f6100aa866198c87e3431a73ab1
pinale <pinale@users.noreply.github.com>
2020-12-27 12:20:54 +0100

    first commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import "../styles.css";
 import { Header, Footer } from "./Layouts/index";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Header />
-      <Footer />
-    </div>
-  );
+export default class extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <Header />
+        <Footer />
+      </Fragment>
+    );
+  }
 }
